6ba5dbbd38fe199e762dba2b51fdc2f359863a68
@arkaroo <arkar.oo@mo.com.mm>
2025-05-19 10:53:37 +0630

    feat: create statefullwidget

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage();
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  late double _deviceHeight, _deviceWidth;
+  _HomePageState();
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Taskly!',
+          style: TextStyle(fontSize: 25, color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+        toolbarHeight: _deviceHeight * 0.15,
+      ),
+    );
+  }
+}
